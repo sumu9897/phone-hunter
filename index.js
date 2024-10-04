87e5a8220f2c0000c3772463a08db00894941b6c
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-10-04 22:42:33 +0600

    First commit for Seach Button Functionality

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Function to load all phones with a log message
 const loadAllPhones = () => {
     console.log("Phones loaded after a 3-second delay.");
+    document.getElementById("spinner").style.display = "none"
 }
 
 // Function to handle search and load phones after a delay
 const handleSearch = () => {
+    document.getElementById("spinner").style.display = "block"
     setTimeout(() => {
         loadAllPhones();
     }, 3000);
