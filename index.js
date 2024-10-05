22c55a634b9fafb652d94c946b913ab165dccd20
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-10-05 08:02:32 +0600

    Small Update

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // Function to load all phones with a log message
 const loadAllPhones = () => {
     console.log("Phones loaded after a 3-second delay.");
-    document.getElementById("spinner").style.display = "none"
+    document.getElementById("spinner").style.display = "none";
+
+    
+
 }
 
 // Function to handle search and load phones after a delay
 const handleSearch = () => {
-    document.getElementById("spinner").style.display = "block"
+    document.getElementById("spinner").style.display = "block";
     setTimeout(() => {
         loadAllPhones();
     }, 3000);
