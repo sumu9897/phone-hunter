dc95bf63f7bde4b7db7b5c0c5e72848902da4f7c
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-10-05 09:01:55 +0600

    Show all api function work done

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 // Function to load all phones with a log message
-const loadAllPhones = () => {
-    console.log("Phones loaded after a 3-second delay.");
+const loadAllPhones = async(status, searchText) => {
+    //console.log(brandName);
     document.getElementById("spinner").style.display = "none";
 
+    // fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`)
+    // .then(res => res.json())
+    // .then(data => {console.log(data)})
+
+    const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText?searchText:"iphone"}`);
+    const data = await response.json();
+    console.log(data)
+    if(status){
+        displayAllPhone(data.data);
+    }else{
+        displayAllPhone(data.data.slice(0,6))
+    }
     
 
+    console.log(status)
+
+}
+
+const displayAllPhone =(phones) =>{
+    const phonesContainer = document.getElementById("phones-container");
+    // for(phone of phones){
+    //     console.log(phone)
+    // }
+
+    phones.forEach(phone =>{
+        console.log(phone)
+    })
+    
 }
 
 // Function to handle search and load phones after a delay
 const handleSearch = () => {
     document.getElementById("spinner").style.display = "block";
+    const searchText= document.getElementById("search-box").value;
     setTimeout(() => {
-        loadAllPhones();
+        loadAllPhones(false,searchText);
     }, 3000);
 }
+
+const handleShowAll =()=>{
+    loadAllPhones(true)
+}
+
+loadAllPhones(false, "iphone")
